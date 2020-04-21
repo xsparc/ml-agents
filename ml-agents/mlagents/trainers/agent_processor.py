4a9915e56d4f4191e0d748710e9ce6d29870141f
Ervin Teng <ervin@unity3d.com>
2020-04-21 11:54:45 -0700

    Print debug message

--- ml-agents/mlagents/trainers/agent_processor.py
+++ ml-agents/mlagents/trainers/agent_processor.py
@@ -271,6 +271,7 @@ class AgentManagerQueue(Generic[T]):
         if the queue is empty.
         """
         try:
+            print("Gettting")
             return self._queue.get(block=block, timeout=timeout)
         except queue.Empty:
             raise self.Empty("The AgentManagerQueue is empty.")
